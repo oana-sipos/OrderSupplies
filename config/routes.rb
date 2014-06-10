b3657c7e5a2ec5f9b2000cc393c930eb31f53314
Oana Sipos <oanasipos@gmail.com>
2014-06-10 09:00:03 +0200

    Fix routing problem for user_signed_in

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ OrderSupplies::Application.routes.draw do
 
   resources :articles
 
+  # devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
   devise_for :users
   root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
