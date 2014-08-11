6f1885556698d35825de835a975bf9273137e4cd
Oana Sipos <oanasipos@gmail.com>
2014-08-11 06:46:40 +0200

    Action Mailer configurations

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,16 @@ module OrderSupplies
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address:                'smtp.gmail.com', 
+        port:                                587,
+        domain:                      'gmail.com',
+        user_name:                 '<oanasipos>',
+        password:                   '<password>',
+        authentication:                  'plain',
+        enable_starttls_auto:                true            
+    }
   end
 end
