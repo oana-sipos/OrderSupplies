2f88b7e64548995b38f1aeca3d72a9c6bad62976
Oana Sipos <oanasipos@gmail.com>
2014-08-14 07:56:45 +0200

    Adding username and password as env vars for deploying

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,8 @@ module OrderSupplies
         address:                'smtp.gmail.com', 
         port:                                587,
         domain:                      'gmail.com',
-        user_name:                 '<oanasipos>',
-        password:                   '<password>',
+        user_name:        ENV['MAILER_USERNAME'],
+        password:         ENV["MAILER_PASSWORD"],
         authentication:                  'plain',
         enable_starttls_auto:                true            
     }
