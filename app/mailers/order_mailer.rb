c6a10c17f6ce7a78fb433066d9d9129e4ee5c8e0
Oana Sipos <oanasipos@gmail.com>
2014-09-14 22:48:03 +0200

    E-mail templates for send_review_order

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,11 @@
+require 'rest_client'
+
 class OrderMailer < ActionMailer::Base
-	def send_for_review_email(user, order, current_user)
+
+  def send_for_review_email(user, order, current_user)
 		@user = user
 		@order = order
-		mail(to: @user.email, from: current_user.email, subject: 'Review the next order')
+		mail(to: @user.email, from: current_user.email, subject: 'Order for UP-nxt')
 	end
 
 	# def send_final_order_email(user, order, current_user)
